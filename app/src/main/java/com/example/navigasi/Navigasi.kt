63b5331111c3c4636ff0a891bfbe817b061113fb
taznimt <taznimanwar06@gmail.com>
2025-11-06 09:15:48 +0700

    tambahkan fungsi Composable DataApp

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,6 +1,19 @@
 package com.example.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+
 enum class Navigasi {
     Formulir,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavController = rememberNavController(),
+    modifier: Modifier
+){
+
 }
